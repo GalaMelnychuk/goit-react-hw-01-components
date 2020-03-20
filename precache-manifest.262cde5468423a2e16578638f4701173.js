45ee0066a29e60fcd72ebe939bcdf4c117a8a51d
Галина <galocka@MacBook-Galina.local>
2020-03-20 23:33:20 +0200

    Updates

---
+++ precache-manifest.262cde5468423a2e16578638f4701173.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c824652668232aad154820700bcf48e",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "ea81bd90cdefcd228950",
+    "url": "/goit-react-hw-01-components/static/css/main.78059ece.chunk.css"
+  },
+  {
+    "revision": "ee240cebaa5cf2d8c66b",
+    "url": "/goit-react-hw-01-components/static/js/2.20179117.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-01-components/static/js/2.20179117.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea81bd90cdefcd228950",
+    "url": "/goit-react-hw-01-components/static/js/main.486d2db3.chunk.js"
+  },
+  {
+    "revision": "a62329f2cc0b24bc37f9",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.39b5137b.js"
+  }
+]);
